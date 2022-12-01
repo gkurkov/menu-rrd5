9acbe7133896c90100791d240bf78538169ca67e
gkurkov <gkurkov@gmail.com>
2022-12-01 22:31:36 +0300

    rrd5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  useRouteMatch,
+  useParams,
+  NavLink,
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <h1>App Layout</h1>
+        <NavLink to='/users'>Users List Page</NavLink>
+        <Switch>
+          <Route path='/users' component={UsersLayout} />
+          <Route path='/' component={MainPage} />
+          <Redirect to='/' />
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+function MainPage() {
+  return <h1>Main Page</h1>
+}
+
+function UsersLayout() {
+  const { path } = useRouteMatch()
+  return (
+    <div>
+      <h1>User Layout</h1>
+      <NavLink to='/'>Main Page</NavLink>
+      <Switch>
+        <Route path={path + '/:userId/profile'} component={UserProfilePage} />
+        <Route path={path + '/:userId/edit'} component={EditUserPage} />
+        <Route path={path} exact component={UsersListPage} />
+        <Redirect from={path + '/:userId'} to={path + '/:userId/profile'} />
+      </Switch>
+    </div>
+  )
+}
+
+function UserProfilePage() {
+  const { userId } = useParams()
+  return (
+    <div>
+      <h1>UserPage</h1>
+      <ul>
+        <li>
+          <NavLink to='/users'>Users List Page</NavLink>
+        </li>
+        <li>
+          <NavLink to={`/users/${userId}/edit`}>Edit List Page</NavLink>
+        </li>
+        <p>userId:{userId}</p>
+      </ul>
+    </div>
+  )
+}
+
+function EditUserPage() {
+  const { userId } = useParams()
+  return (
+    <div>
+      <h1>Edit User Page</h1>
+      <ul>
+        <li>
+          <NavLink to={'/users/' + userId}>User Profile Page</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/users/' + (+userId + 1)}>Another User</NavLink>
+        </li>
+        <li>
+          <NavLink to='/users'>Users List Page</NavLink>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+function UsersListPage() {
+  const { path } = useRouteMatch()
+  return (
+    <div>
+      <h1>Users List Page</h1>
+      <ul>
+        {new Array(5).fill('').map((_, index) => (
+          <li key={'users_list_component_' + index}>
+            <NavLink to={`${path}/${index}`}>User {index}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
